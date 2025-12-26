e1684262b662e818b297699ced66f2b5fc1ae540
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 04:20:14 -0700

    Restructure

--- modules/hosts.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, ... }:
-{
-  flake.nixosConfigurations.my-host = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = with inputs.self.modules.nixos; [ vm-configuration vm-hardware-configuration ];
-  };
-}
