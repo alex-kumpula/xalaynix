44ce43d3b6be12f89fbb80aea86707aae91d95d7
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 00:22:08 -0700

    Fix

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
-  flake.nixosConfigurations.my-host2 = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.my-host = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = with inputs.self.nixosModules; [ vm-configuration vm-hardware-configuration ];
+    modules = with inputs.self.modules.nixos; [ vm-configuration vm-hardware-configuration ];
   };
 }
