84bece4613c3d9815a4bd17ae43fb7d24e8ea57f
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 03:21:23 -0700

    Test

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -10,6 +10,32 @@
   ];
 
 
+  flake.homeConfigurations = {
+    # FIXME - DONE replace with your username@hostname
+    "alex" = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+
+      modules = [
+      ];
+    };
+  };
+
+  # homeConfigurations = {
+  #     # FIXME - DONE replace with your username@hostname
+  #     "alex" = home-manager.lib.homeManagerConfiguration {
+  #       pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+  #       extraSpecialArgs = mySpecialArgs;
+  #       modules = [
+  #         # > Our main home-manager configuration file <
+  #         ./homes/alex/home.nix
+  #         {
+  #           home.packages = [affinity-nix.packages.x86_64-linux.v3];
+  #         }
+  #       ];
+  #     };
+  #   };
+
+
   # Required to define `homeConfigurations` in multiple files.
   # Otherwise:
   #   The option `flake.homeConfigurations' is defined multiple times while it's expected to be unique.
