9a36b8d1bb12f19db0cf3c3022a0d8a980356782
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 00:18:37 -0700

    Test nixosModules

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -2,6 +2,6 @@
 {
   flake.nixosConfigurations.my-host = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = with inputs.self.modules.nixos; [ vm-configuration vm-hardware-configuration ];
+    modules = with inputs.self.nixosModules; [ vm-configuration vm-hardware-configuration ];
   };
 }
