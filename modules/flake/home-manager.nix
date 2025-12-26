cf0f549c22c0f1eec24415f86c5c66be6953199d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 03:18:36 -0700

    Update

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -5,9 +5,9 @@
     inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  imports = [
-    inputs.home-manager.flakeModules.home-manager
-  ];
+  # imports = [
+  #   inputs.home-manager.flakeModules.home-manager
+  # ];
 
 
   # Required to define `homeConfigurations` in multiple files.
