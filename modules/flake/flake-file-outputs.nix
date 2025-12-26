b38f7c8b7674315bb896389162c2a7315393955f
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 04:11:39 -0700

    test

--- modules/flake/flake-file-outputs.nix
+++ modules/flake/flake-file-outputs.nix
@@ -1,4 +1,4 @@
 { lib, ... }:
 {
-  flake-file.outputs = lib.mkForce "inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)";
+  flake-file.outputs = lib.mkForce "inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules) (inputs.import-tree ./outputs)";
 }
