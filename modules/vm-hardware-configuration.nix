c4669aeb7d941a706de0ed82df18f8b6bb46821b
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-25 23:03:21 -0700

    Add vm configuration files

---
+++ modules/vm-hardware-configuration.nix
@@ -0,0 +1,28 @@
+
+{
+  config,
+  ...
+}:
+{
+  flake.modules.nixos.vm-hardware-configuration =
+  { config, lib, pkgs, modulesPath, ... }:
+  {
+    imports =
+      [ (modulesPath + "/profiles/qemu-guest.nix")
+      ];
+  
+    boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+  
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/f5576cec-052d-4bb2-91df-c369c73a2034";
+        fsType = "ext4";
+      };
+  
+    swapDevices = [ ];
+  
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+}
