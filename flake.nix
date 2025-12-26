08b23f0976215b1f1f8aa303db3a65050e8a22c3
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-26 03:18:56 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -19,6 +19,10 @@
       inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
     };
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.11";
+    };
     import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-lib.follows = "nixpkgs";
