01321a4b717dbfa2b05b8aa6dcb7153b08a27190
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-28 03:08:52 -0700

    Test

--- src/nixosConfigurations/alex-vm-001/default.nix
+++ src/nixosConfigurations/alex-vm-001/default.nix
@@ -2,6 +2,11 @@
 {
   flake.nixosConfigurations.alex-vm-001 = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = with inputs.self.modules.nixos; [ vm-configuration vm-hardware-configuration ];
+    modules = with inputs.self.modules.nixos; [ 
+      vm-configuration 
+      vm-hardware-configuration 
+    ] ++ [
+      ./_disko.nix
+    ];
   };
 }
