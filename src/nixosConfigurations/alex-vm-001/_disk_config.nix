81aaaf2196760e34c14b51531da970aedf85a7d2
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-28 23:23:49 -0700

    update

--- src/nixosConfigurations/alex-vm-001/_disk_config.nix
+++ src/nixosConfigurations/alex-vm-001/_disk_config.nix
@@ -3,48 +3,73 @@ let
   device = "/dev/vda";
 in
 {
-  boot.initrd.systemd.services.recreate-root = {
-    description = "Rolling over and creating new filesystem root";
+  # boot.initrd.systemd.services.recreate-root = {
+  #   description = "Rolling over and creating new filesystem root";
 
-    wantedBy = [ "initrd.target" ];
-    requires = [ "initrd-root-device.target" ];
-    after = [
-      "initrd-root-device.target"
-      "local-fs-pre.target"
-    ];
-    before = [
-      "sysroot.mount"
-      "create-needed-for-boot-dirs.service"
-    ];
+  #   wantedBy = [ "initrd.target" ];
+  #   requires = [ "initrd-root-device.target" ];
+  #   after = [
+  #     "initrd-root-device.target"
+  #     "local-fs-pre.target"
+  #   ];
+  #   before = [
+  #     "sysroot.mount"
+  #     "create-needed-for-boot-dirs.service"
+  #   ];
 
-    unitConfig.DefaultDependencies = "no";
-    serviceConfig.Type = "oneshot";
+  #   unitConfig.DefaultDependencies = "no";
+  #   serviceConfig.Type = "oneshot";
 
-    script = ''
-      mkdir /btrfs_tmp
-      mount /dev/root_vg/root /btrfs_tmp
-      if [[ -e /btrfs_tmp/root ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+  #   script = ''
+  #     mkdir /btrfs_tmp
+  #     mount /dev/root_vg/root /btrfs_tmp
+  #     if [[ -e /btrfs_tmp/root ]]; then
+  #         mkdir -p /btrfs_tmp/old_roots
+  #         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+  #         mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+  #     fi
 
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-      }
+  #     delete_subvolume_recursively() {
+  #         IFS=$'\n'
+  #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+  #             delete_subvolume_recursively "/btrfs_tmp/$i"
+  #         done
+  #         btrfs subvolume delete "$1"
+  #     }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
+  #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+  #         delete_subvolume_recursively "$i"
+  #     done
 
-      btrfs subvolume create /btrfs_tmp/root
-      umount /btrfs_tmp
-    '';
-  };
+  #     btrfs subvolume create /btrfs_tmp/root
+  #     umount /btrfs_tmp
+  #   '';
+  # };
+
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/root_vg/root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
   # fileSystems."/" = {
   #   device = "/dev/root_vg/root";
