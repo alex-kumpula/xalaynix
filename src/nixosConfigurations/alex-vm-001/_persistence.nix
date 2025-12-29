0b059f7740280c286e8c27a3d0ea11b48e8f16e9
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-28 21:35:03 -0700

    update

--- src/nixosConfigurations/alex-vm-001/_persistence.nix
+++ src/nixosConfigurations/alex-vm-001/_persistence.nix
@@ -8,8 +8,8 @@
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
-      # "/etc/NetworkManager/system-connections"
-      # { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+      "/etc/NetworkManager/system-connections"
+      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     # files = [
     #   "/etc/machine-id"
