4cfb1550dbebfb3b4a37134d1177a812421f2eac
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-29 18:03:45 -0700

    Fix

--- src/modules/git.nix
+++ src/modules/git.nix
@@ -1,25 +1,29 @@
-{ inputs, config, lib, ... }:
+{ config, lib, ... }:
 let
   cfg = config.flakeConfig.git;
+
+  gitModule = {
+    options.flakeConfig.git = {
+      userName = lib.mkOption {
+        type = lib.types.str;
+        default = "default";
+        description = "The name to use in git commits.";
+      };
+      userEmail = lib.mkOption {
+        type = lib.types.str;
+        default = "default@example.com";
+        description = "The email to use in git commits.";
+      };
+    };
+
+    config = {
+      programs.git.enable = true;
+      programs.git.userName = cfg.userName;
+      programs.git.userEmail = cfg.userEmail;
+    };
+  };
+
 in
 {
-  options.flakeConfig.git = {
-    userName = lib.mkOption {
-      type = lib.types.str;
-      default = "default";
-      description = "The name to use in git commits.";
-    };
-
-    userEmail = lib.mkOption {
-      type = lib.types.str;
-      default = "default@example.com";
-      description = "The email to use in git commits.";
-    };
-  };
-
-  flake.modules.nixos.git = {
-    programs.git.enable = true;
-    programs.git.userName = cfg.userName;
-    programs.git.userEmail = cfg.userEmail;
-  };
+  flake.modules.nixos.git = gitModule;
 }
