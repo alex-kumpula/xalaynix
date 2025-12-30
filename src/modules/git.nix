858e14a11715caa56a3fd7f20757f927c5a63552
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-29 19:21:49 -0700

    test

--- src/modules/git.nix
+++ src/modules/git.nix
@@ -5,24 +5,16 @@
     { config, lib, ... }: 
     {
     options.test123.git = {
-      userName = lib.mkOption {
-        type = lib.types.str;
-        default = "default";
-        description = "Git user name";
-      };
-      
-      userEmail = lib.mkOption {
-        type = lib.types.str;
-        default = "default@example.com";
-        description = "Git user email";
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable simple git configuration.";
       };
     };
 
     config = {
       programs.git = {
-        enable = true;
-        userName = config.test123.git.userName;
-        userEmail = config.test123.git.userEmail;
+        enable = config.test123.git.enable;
       };
     };
   };
