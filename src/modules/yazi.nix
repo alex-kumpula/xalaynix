eb5894b4760a202d85d7b74a7c96719ce16e7e4b
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-29 21:08:55 -0700

    Test

--- src/modules/yazi.nix
+++ src/modules/yazi.nix
@@ -10,27 +10,27 @@
           enableZshIntegration = true;
         };
 
-        # programs.bash = {
-        #   # Appended to ~/.bashrc
-        #   bashrcExtra = ''
-        #     # Custom stuff here
-        #     alias ll='ls -lah'
+        programs.bash = {
+          # Appended to ~/.bashrc
+          bashrcExtra = ''
+            # Custom stuff here
+            alias ll='ls -lah'
 
-        #     # Yazi shell wrapper (see the Yazi website Getting Started page)
-        #     function y() {
-        #       local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-        #       yazi "$@" --cwd-file="$tmp"
-        #       IFS= read -r -d '\0' cwd < "$tmp"
-        #       [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-        #       rm -f -- "$tmp"
-        #     }
-        #   '';
+            # Yazi shell wrapper (see the Yazi website Getting Started page)
+            function y() {
+              local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+              yazi "$@" --cwd-file="$tmp"
+              IFS= read -r -d '\0' cwd < "$tmp"
+              [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+              rm -f -- "$tmp"
+            }
+          '';
 
-        #   # Appended to ~/.bash_profile
-        #   profileExtra = ''
-        #     export EDITOR=nano
-        #   '';
-        # };
+          # Appended to ~/.bash_profile
+          profileExtra = ''
+            export EDITOR=nano
+          '';
+        };
       };
     };
 }
