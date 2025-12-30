33a0c18da2685048e0d04703dc6b05e1d3abfc6c
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-29 22:55:52 -0700

    test

--- src/nixosConfigurations/alex-vm-001/_disk_config.nix
+++ src/nixosConfigurations/alex-vm-001/_disk_config.nix
@@ -85,17 +85,13 @@ in
     };
   };
 
+  boot.initrd.systemd.enable = true;
   boot.tmp.cleanOnBoot = true;
   services.lvm.enable = true;
 
   fileSystems."/persistent" = {
-    # device = "/dev/root_vg/root";
     neededForBoot = true;
-    # fsType = "btrfs";
-    # options = [ "subvol=persist" ];
   };
-
-  boot.initrd.systemd.enable = true;
 
   boot.initrd.systemd.services.debug-log = {
     description = "Initrd debug check";
@@ -126,73 +122,13 @@ in
     ];
   };
 
-  # boot.initrd.systemd.services.recreate-root = {
-  #   description = "Rolling over and creating new filesystem root";
-
-  #   wantedBy = [ "initrd.target" ];
-  #   requires = [ "initrd-root-device.target" ];
-  #   after = [
-  #     "initrd-root-device.target"
-  #     "local-fs-pre.target"
-  #   ];
-  #   before = [
-  #     "sysroot.mount"
-  #     "create-needed-for-boot-dirs.service"
-  #   ];
-
-  #   unitConfig.DefaultDependencies = "no";
-  #   serviceConfig.Type = "oneshot";
-
-  #   script = ''
-  #     mkdir /btrfs_tmp
-  #     mount /dev/root_vg/root /btrfs_tmp
-  #     if [[ -e /btrfs_tmp/root ]]; then
-  #         mkdir -p /btrfs_tmp/old_roots
-  #         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #         mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #     fi
-
-  #     delete_subvolume_recursively() {
-  #         IFS=$'\n'
-  #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #             delete_subvolume_recursively "/btrfs_tmp/$i"
-  #         done
-  #         btrfs subvolume delete "$1"
-  #     }
-
-  #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-  #         delete_subvolume_recursively "$i"
-  #     done
-
-  #     btrfs subvolume create /btrfs_tmp/root
-  #     umount /btrfs_tmp
-  #   '';
-  # };
-
-  # boot.initrd.postResumeCommands = lib.mkAfter ''
-  #   mkdir /btrfs_tmp
-  #   mount /dev/root_vg/root /btrfs_tmp
-  #   if [[ -e /btrfs_tmp/root ]]; then
-  #       mkdir -p /btrfs_tmp/old_roots
-  #       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #       mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #   fi
-
-  #   delete_subvolume_recursively() {
-  #       IFS=$'\n'
-  #       for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #           delete_subvolume_recursively "/btrfs_tmp/$i"
-  #       done
-  #       btrfs subvolume delete "$1"
-  #   }
-
-  #   for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-  #       delete_subvolume_recursively "$i"
-  #   done
-
-  #   btrfs subvolume create /btrfs_tmp/root
-  #   umount /btrfs_tmp
-  # '';
+  environment.persistence."/persistent/home" = {
+    enable = true;
+    hideMounts = true;
+    directories = [
+      "/home"
+    ];
+  };
 
   disko.devices = {
     disk.main = {
