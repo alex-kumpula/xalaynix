6b12e2988820fcd71ead3a0b92344ad3008c0ade
Alex Kumpula <alex.kumpula01@gmail.com>
2025-12-29 19:27:25 -0700

    test

--- src/modules/git.nix
+++ src/modules/git.nix
@@ -1,21 +1,26 @@
 # ./modules/git-simple.nix
 { self, lib, ... }:  # flake-parts module
 {
-  flake.modules.nixos.git = 
-    { config, lib, ... }: 
-    {
-    options.test123.git = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = "Enable simple git configuration.";
-      };
-    };
+  # flake.modules.nixos.git = { 
+  #   config, lib, ... 
+  #   }: 
+  #   {
+  #     options.test123.git = {
+  #       enable = lib.mkOption {
+  #         type = lib.types.bool;
+  #         default = true;
+  #         description = "Enable simple git configuration.";
+  #       };
+  #     };
 
-    config = {
-      programs.git = {
-        enable = config.test123.git.enable;
-      };
-    };
+  #     config = {
+  #       programs.git = {
+  #         enable = config.test123.git.enable;
+  #       };
+  #     };
+  # };
+
+  flake.modules.nixos.git = {
+    programs.git.enable = true;
   };
 }
--- src/nixosConfigurations/alex-vm-001/_config.nix
+++ src/nixosConfigurations/alex-vm-001/_config.nix
@@ -1,7 +1,6 @@
 { inputs, config, lib, ... }:
 {
   flake.modules.nixos.hosts.alex-vm-001 = {
-    flakeConfig.git.userName = "Alex Kumpula";
-    flakeConfig.git.userEmail = "alex.kumpula@example.com";
+    
   };
 }
