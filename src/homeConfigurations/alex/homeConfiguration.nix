a95098ac504a3619685cae8771a9827cc485847e
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-07 23:19:12 -0700

    test

--- src/homeConfigurations/alex/homeConfiguration.nix
+++ src/homeConfigurations/alex/homeConfiguration.nix
@@ -6,6 +6,7 @@
     modules = with config.flake.modules.homeManager; [
       alex
     ] ++ [
+      inputs.xalaynix.modules.home-manager.xalaynix
       inputs.xalaynixDesktop.modules.homeManager.xalaynixDesktop
     ];
   };
