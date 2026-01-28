29b936095698d5ff8e3353205389d9b39c2b4b26
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-27 19:49:29 -0700

    fix

--- src/nixosConfigurations/proxmox-nixos-host-001/impermanence.nix
+++ src/nixosConfigurations/proxmox-nixos-host-001/impermanence.nix
@@ -11,10 +11,10 @@
       enable = true;
       hideMounts = true;
       directories = config.xalaynix.constants.commonPersistentDirectories ++ [
-        "var/lib/machines"  # Persist containers and VMs
-        "var/lib/libvirt"   # Persist libvirt data
-        "etc/libvirt"       # Persist libvirt config
-        "etc/ssh"           # Persist SSH config and keys
+        "/var/lib/machines"  # Persist containers and VMs
+        "/var/lib/libvirt"   # Persist libvirt data
+        "/etc/libvirt"       # Persist libvirt config
+        "/etc/ssh"           # Persist SSH config and keys
       ];
       files = config.xalaynix.constants.commonPersistentFiles;
     };
