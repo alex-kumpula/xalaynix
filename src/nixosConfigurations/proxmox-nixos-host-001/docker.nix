f301966e65f0b1e68aca5f8857b7095184e2df76
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-27 21:18:59 -0700

    test

--- src/nixosConfigurations/proxmox-nixos-host-001/docker.nix
+++ src/nixosConfigurations/proxmox-nixos-host-001/docker.nix
@@ -4,17 +4,16 @@
     # See https://wiki.nixos.org/wiki/Docker
 
     virtualisation.docker = {
-      # Consider disabling the system wide Docker daemon
       enable = true;
-
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-        # Optionally customize rootless Docker daemon settings
-        daemon.settings = {
-          dns = [ "1.1.1.1" "8.8.8.8" ];
-          registry-mirrors = [ "https://mirror.gcr.io" ];
-        };
+      # Set up resource limits
+      daemon.settings = {
+        experimental = true;
+        default-address-pools = [
+          {
+            base = "172.30.0.0/16";
+            size = 24;
+          }
+        ];
       };
     };
 
